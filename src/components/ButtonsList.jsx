6466d41e2081d58aa286527037f65028ec563537
dineshsrisai <dineshsrisai1@gmail.com>
2026-02-09 19:17:20 +0530

    Improved using map

--- src/components/ButtonsList.jsx
+++ src/components/ButtonsList.jsx
@@ -1,21 +1,26 @@
 import Button from "./Button";
 
+const list = [
+  "All",
+  "Live",
+  "Cricket",
+  "Cinema",
+  "Web Development",
+  "Gaming",
+  "News",
+  "Cooking",
+  "Podcasts",
+  "Mixes",
+  "Recently uploaded",
+  "New to you",
+];
+
 const ButtonsList = () => {
   return (
     <div className="flex text-white cursor-pointer">
-      <Button name="All" />
-      <Button name="Live" />
-      <Button name="Cricket" />
-      <Button name="Cinema" />
-      <Button name="Web Development" />
-      <Button name="Gaming" />
-      <Button name="News" />
-      <Button name="Cooking" />
-      <Button name="Podcasts" />
-      <Button name="Mixes" />
-      <Button name="Recently uploaded" />
-
-      <Button name="New to you" />
+      {list.map((name) => (
+        <Button name={name} />
+      ))}
     </div>
   );
 };
