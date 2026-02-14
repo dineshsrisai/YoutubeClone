c5c135b67bee2e121c4c7cf14265413b4c7367ba
dineshsrisai <dineshsrisai1@gmail.com>
2026-02-14 22:51:18 +0530

    Dynamic

--- src/components/WatchPage.jsx
+++ src/components/WatchPage.jsx
@@ -1,5 +1,30 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
+
 const WatchPage = () => {
-  return <div className="">WatchPage</div>;
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+  return (
+    <div className="pl-4 pt-0">
+      <iframe
+        className=" rounded-xl"
+        width="1100"
+        height="580"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;
